6455fa1c85304170c969a46e1e97e6d4a9583ae6
luncheon <takeuck+github@gmail.com>
2017-11-17 18:04:31 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Highlight from 'highlight.js/lib/highlight.js'
+import 'highlight.js/styles/zenburn.css'
+
+import Reveal from 'reveal.js'
+import 'reveal.js/css/reveal.css'
+import 'reveal.js/css/theme/black.css'
+
+import './style.css'
+
+Highlight.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
+Highlight.initHighlightingOnLoad()
+Reveal.initialize({ slideNumber: 'c/t' })
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const ExtractTextPlugin             = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin             = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+
+const path = require('path')
+const resolvePath = filename => path.resolve(__dirname, filename)
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolvePath('docs'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: { loader: 'css-loader', options: { import: false } }
+        }),
+      },
+      {
+        test: /\.pug$/,
+        use: { loader: 'pug-loader' },
+      },
+      {
+        test: /\.(eot|woff2?|ttf|svg)(\?.*)?$/,
+        use: { loader: 'url-loader' },
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.pug',
+      inlineSource: '.(js|css)$',
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+  ],
+}
